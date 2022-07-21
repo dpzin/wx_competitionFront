74c7dd3094e7c39435e9a73e19eb8d75664d27b7
gaofei <gaofei@huayun.com>
2022-07-21 21:45:58 +0800

    微信支付草稿

--- pages/competation/index.js
+++ pages/competation/index.js
@@ -26,6 +26,42 @@ Page({
         console.log(res.data)
       }
     })
+    // 微信支付下单
+    // wx.request({
+    //   url: 'https://www.supboogie.top/bjss/createOrder',
+    //   method: 'POST',
+    //   data: {
+    //     "openId":"o-T0A5pcEnNOciFf2Lco1evFvCdw",
+    //     "productTypeEnum":"ENTRY_FEE",
+    //     "productIds":["2c9abe3f7fc1d0af017fc1d8eaa30008"]
+    //   },
+    //   success: (res)=> {
+    //     console.log(res.data)
+    //     //this.pay(res.data.data)
+    //   }
+    // })
+  },
+  pay(data) {
+    console.log("---",data)
+    var request = {
+
+    }
+    wx.requestPayment({
+		"timeStamp": data.timeStamp.toString(),
+		"nonceStr": data.nonceStr,
+		"package": "prepay_id="+data.package,
+		"signType": "RSA",
+		"paySign": data.paySign,
+		"success":function(res){
+      console.log(res)
+    },
+		"fail":function(res){
+      console.log(res)
+    },
+		"complete":function(res){
+      console.log(res)
+    }
+	  })
   },
   toDetail() {
     console.log(123)
