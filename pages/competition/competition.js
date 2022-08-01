6172cb17a65d5491d07a42d8e1990037c314fbd6
gaofei <gaofeidoit@163.com>
2022-08-01 23:55:18 +0800

    新增用户授权

--- pages/competition/competition.js
+++ pages/competition/competition.js
@@ -29,9 +29,83 @@ Page({
       }
     })
   },
+  getUserInfo() {
+    let flag = false;
+    var userInfo = wx.getStorageSync("myUserInfo")
+    if(!userInfo) {
+      wx.getUserProfile({
+        desc: '使用小程序内的功能及服务',
+        success: (res) => {
+          flag = true;
+          console.log(res)
+          // this.setData({
+          //   userInfo: res.userInfo,
+          //   hasUserInfo: true
+          // })
+        },
+        fail: (res)=> {
+          console.log(res)
+        }
+      })
+    }
+    console.log(333)
+   return flag;
+  },
   toDetail(event) {
-    wx.navigateTo({
-      url: `/pages/competitionDetail/competitionDetail?id=${event.currentTarget.dataset.id}`,
+    wx.clearStorage({
+      success: (res) => {},
+    })
+    var userInfo = wx.getStorageSync("myUserInfo")
+    if(!userInfo) {
+      wx.getUserProfile({
+        desc: '使用小程序内的功能及服务',
+        success: (res) => {
+          debugger
+          wx.setStorageSync('myUserInfo', res.userInfo)
+          console.log(res)
+          let userInfo = {
+            "nickName": res.userInfo.nickName,
+            "gender": res.userInfo.gender,
+            "avatarUrl": res.userInfo.avatarUrl,
+            "role":"common"
+          }
+          //获取openId
+          wx.login({
+            success: async(res) => {
+              wx.request({
+                url: 'https://www.supboogie.top/bjss/wx/getOpenId',
+                method: 'POST',
+                data: {
+                  "code": res.code
+                },
+                success: (res)=> {
+                  const openId = res.data.data.openid
+                  userInfo.openId = openId
+                  this.addOrUpdateWxUser(userInfo)
+                }
+              })
+            }
+          })
+          wx.navigateTo({
+            url: `/pages/competitionDetail/competitionDetail?id=${event.currentTarget.dataset.id}`,
+          })
+        },
+        fail: (res)=> {
+          console.log(res)
+        }
+      })
+    } else {
+      wx.navigateTo({
+        url: `/pages/competitionDetail/competitionDetail?id=${event.currentTarget.dataset.id}`,
+      })
+    }
+  },
+  
+  addOrUpdateWxUser(userInfo) {
+    wx.request({
+      url: 'https://www.supboogie.top/bjss/addOrUpdateWXUser',
+      method: "POST",
+      data: userInfo
     })
   },
   /**
