9379b65e1b6c321a942972907d6629d5ee890679
zhangqiuya <zhangqiuya@k12cloud.cn>
2022-08-05 23:14:16 +0800

    添加状态

--- pages/competitionDetail/competitionDetail.js
+++ pages/competitionDetail/competitionDetail.js
@@ -27,25 +27,15 @@ Page({
     detail: {},
     roleDetail: {},
     role:"COMMON",
-    buttonStr:"立即报名",
-    competitionId:""
+    buttonStr:"",
+    competitionId:"",
+    btnDisabled: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    wx.request({
-      url: 'https://www.supboogie.top/bjss/getCompetition',
-      method: 'POST',
-      data: {id: options.id},
-      success: (res) => {
-        this.setData({
-          detail: res.data.data,
-          query: options
-        })
-      }
-    })
     let userInfo = wx.getStorageSync('myUserInfo')
     let data = {
       "competitionId":options.id,
@@ -53,20 +43,30 @@ Page({
     }
     this.competitionId = options.id
     post("https://www.supboogie.top/bjss/getRole",data).then((res)=>{
-      console.log(res)
       this.roleDetail = res.data.roleDetail
       this.role = res.data.role
-      this.convert()
+      wx.request({
+        url: 'https://www.supboogie.top/bjss/getCompetition',
+        method: 'POST',
+        data: {id: options.id},
+        success: (res) => {
+          this.setData({
+            detail: res.data.data,
+            query: options,
+            btnDisabled: res.data.data.status === '0' || res.data.data.status=== '3' ? true : false
+          })
+          this.convert()
+        }
+      })
     })
   },
   toSign() {
-    if(this.role == "COMMON") {
+    if(!this.data.btnDisabled && this.role == "COMMON") {
       wx.navigateTo({
         url: `/pages/sign/sign?id=${this.data.query.id}`,
       })
     }
-    if(this.role ==  "MC") {
-      console.log("MC",this.competitionId)
+    if(!this.data.btnDisabled && this.role ==  "MC") {
       wx.navigateTo({
         url: `/pages/controller/controller?competitionId=${this.competitionId}`,
       })
@@ -77,8 +77,12 @@ Page({
       this.buttonStr = "大屏遥控"
     } else if(this.role == "JUDGE") {
       this.buttonStr = "海选打分"
+    } else if (this.data.detail.status === '0') {
+      this.buttonStr = "报名未开始"
+    } else if (this.data.detail.status === '3') {
+      this.buttonStr = "报名已结束"
     } else {
-      this.buttonStr = "立即报名"
+      this.buttonStr = '立即报名'
     }
     this.setData({
       buttonStr: this.buttonStr
